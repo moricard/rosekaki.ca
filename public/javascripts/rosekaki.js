b4dd8973d5fbe7f8e4a0a3094b5ffa903a8372aa
ricja <marco.ricard@gmail.com>
2013-02-24 15:24:44 -0500

    logo au top

--- public/javascripts/rosekaki.js
+++ public/javascripts/rosekaki.js
@@ -13,11 +13,18 @@ $(document).ready(function() {
 
     //Smooth scrolling
     $('.scrollpage').click(function() {
+
         var elementClicked = $(this).attr("href");
+
         var destination = $(elementClicked).offset().top;
-        $('html:not(:animated),body:not(:animated)').animate({ scrollTop: destination - 200 }, 1200, function() {
-            window.location.hash = elementClicked;
-        });
+
+        $('html:not(:animated),body:not(:animated)')
+            .animate({
+                scrollTop: destination - 80 
+            }, 1200
+             , function() {
+                 window.location.hash = elementClicked;
+             });
         
         return false;
     }); 
@@ -29,38 +36,14 @@ $(document).ready(function() {
 
         var scrollTop = getScrollTop()
         ,   logoOffset = logo.offset().top
-        ,   navOffset = Math.min( 120, (scrollTop *0.2) - 50);
+        ,   navOffset = Math.min( 0, (scrollTop *0.2) - 50);
 
         subLogo.css({
             'top' : (scrollTop * 0.6 + 88) + 'px'
         });
 
-        if ( !fixed ) {
-            logo.css({'top' : (scrollTop * 0.6) + 'px'});
-            if (scrollTop >= logoOffset ){
-                logo.css({
-                    'position' : 'fixed'
-                    , 'top'      : 0
-                });            
-                fixed = !fixed;
-            }
+        logo.css({'top' : (scrollTop * 0.6) + 'px'});
 
-        } else if ( fixed && scrollTop < 500 ) {
-            logo.css('position', 'relative');
-            fixed = !fixed;
-        }
-
-/*
-        if ( !(fixed = (scrollTop >= logoOffset)) ) {
-            logo.css({'top' : (scrollTop * 0.6) + 'px'});
-        } else if ( fixed && scrollTop < 450 ) {
-            logo.css('position', 'relative');
-        } else if ( scrollTop >= 450){
-            logo.css('position','fixed')
-                .css('top', 0);
-        }
-        console.log(fixed);
-*/
         nav.css({'top' : navOffset + 'px' });
 	
     });
